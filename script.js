14acf38c3392990d34e11f31437389ce0c5ad500
ashesVel <ah5008034@gmail.com>
2024-09-23 19:56:33 -0600

    a

---
+++ script.js
@@ -0,0 +1,32 @@
+//18 09 24 / Hernandez Velazquez Ashley Valentina
+//comentario en una sola línea
+/*
+comentario
+multi 
+lineas
+*/
+//Variables
+/*
+las variables guardan información
+que puedes utilizar
+*/
+//Declaración de variable
+let nombre="Ashley";
+const pi=3.1416;
+var edad=25;
+/*tipos de datos
+cadena de texto (string): "Hola", 'mundo'
+Numero (number): 5,3,14
+Booleanos (bollean): true, false
+*/
+//ejemplo
+let mensaje='Hola mundo';
+let numero=42;
+let estudiante=true;
+//operadores matemáticos
+let a=5;
+let b=2;
+let suma=a+b;//7
+let resta=a-b;//3
+let multiplicacion=a*b;//10
+let division=a/b;//2.5
